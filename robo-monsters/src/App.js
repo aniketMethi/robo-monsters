aae5b9b377a4d0f353f00b1bc08a26f97b944f84
github5786 <aniketmethi@gmail.com>
2021-04-18 03:56:01 +0530

    fetching content

--- robo-monsters/src/App.js
+++ robo-monsters/src/App.js
@@ -7,34 +7,22 @@ export default class App extends Component {
     super(props)
   
     this.state = {
-      monsters : [
-        {
-          id : "1",
-          name : "ghost"
-        },
-        {
-          id : "2",
-          name : "vampire"
-        },
-        {
-          id : "3",
-          name : "zombie"
-        },
-        {
-          id : "4",
-          name : "dracula"
-        }
-
-      ]
+      monsters : []
     }
   }
   
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters:users}));
+  }
  
   render() {
     return (
       <div className = 'App'>
         {this.state.monsters.map(monster => <h1 key = {monster.id}>{monster.name}</h1>)}
       </div>
+      
     )
   }
-}
+} 
